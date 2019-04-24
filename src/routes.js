ff25625a0bc732f4d9a3418ed0b1ab08fcf5832c
Philip Nunoo <phanunoo@gmail.com>
2019-04-24 07:00:56 +0000

    Added switch

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from 'pages/Home/Home';
 import Result from 'pages/Result';
 import Welcome from './pages/Welcome';
 
-const Routes = () => ([
-    <Route key="welcome" exact path="/" component={Welcome} />,
-    <Route key="gender" path="/(female|male)/" component={Home}/>,
-    <Route key="result" exact path="/results" component={Result}/>,
-    <Route key="notFound" path="*" component={() => 'Not Found'}/>,
-]);
+const Routes = () => (
+    <Switch>
+        <Route key="welcome" exact path="/" component={Welcome} />,
+        <Route key="gender" path="/(female|male)/" component={Home}/>,
+        <Route key="result" exact path="/results" component={Result}/>,
+        <Route key="notFound" component={() => 'Not Found'}/>,
+    </Switch>
+);
 
 export default Routes;
